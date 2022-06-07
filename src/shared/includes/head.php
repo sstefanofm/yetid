d3b0bee94780a6eb1cf76ecae3807c40341b5aca
stf <sstefanofm@gmail.com>
2022-06-07 00:07:15 -0300

    Navbar with styles.

--- src/shared/includes/head.php
+++ src/shared/includes/head.php
@@ -4,4 +4,10 @@
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
+
+  <!-- Google fonts -->
+  <!-- Metamorphous & Special Elite -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Metamorphous&family=Special+Elite&display=swap" rel="stylesheet">
+  <!-- -->
