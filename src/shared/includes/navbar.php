b45fdc4734937b7af569c8b3eb7111adaa05b686
stf <sstefanofm@gmail.com>
2022-06-08 21:46:31 -0300

    Navbar brand redirects to main page.

--- src/shared/includes/navbar.php
+++ src/shared/includes/navbar.php
@@ -2,12 +2,12 @@
   <div class="container">
     <div class="row justify-content-between">
       <div class="col-auto me-auto">
-        <a class="navbar-brand" href="#">Yetid.</a>
+        <a class="navbar-brand" href="http://localhost/stf/yetid/src/">Yetid.</a>
       </div>
 
       <div class="col-auto">
-        <a class="btn red" href="routes/register/register.php">Register</a>
-        <a class="btn btn-green" href="routes/login/login.php">Login</a>
+        <a class="btn red" href="http://localhost/stf/yetid/src/routes/register/register.php">Register</a>
+        <a class="btn btn-green" href="http://localhost/stf/yetid/src/routes/login/login.php">Login</a>
       </div>
     </div>
   </div>
