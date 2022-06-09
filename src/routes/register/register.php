114c7eb0505831b7e283829bf9fa274d782a636d
stf <sstefanofm@gmail.com>
2022-06-08 21:48:20 -0300

    Finished register styles.

--- src/routes/register/register.php
+++ src/routes/register/register.php
@@ -12,21 +12,62 @@ include '../../shared/view/renderer.php';
 <?php
 include '../../shared/includes/head.php';
 ?>
+<!-- My styles -->
+<link rel="stylesheet" href="../../css/navbar_styles.css" media="all">
+<link rel="stylesheet" href="../../css/body_styles.css" media="all">
+<link rel="stylesheet" href="css/register_styles.css" media="all">
+</head>
 
 <body>
+  <?php
+  include '../../shared/includes/navbar.php';
+  ?>
+
   <?php
   render_alert($_SESSION['success'], $_SESSION['message']);
   ?>
 
-  <form action="controller/save_user.php" method="POST">
-    <input type="text" name="username"> <br>
-    <input type="password" name="password"> <br>
-    <input type="submit" name="submit"> <br>
-  </form>
+  <div class="row">
+    <div class="col-3"></div>
+
+    <div class="col-md-4 card card-body grey-card m-4 row">
+      <div class="m-4 m-auto">
+        <h2 class="yetid-font">Sign up</h2>
+        <form action="controller/save_user.php" method="POST">
+          <div class="form-group">
+            <label class="form-label" for="username">Username</label> <br>
+            <input class="username" type="text" name="username"> <br>
+          </div>
+
+          <div class="form-group">
+            <label class="form-label" for="password">Password</label> <br>
+            <input class="password" type="password" name="password"> <br>
+            <div class="checkbox">
+              <input class="password-checkbox" type="checkbox" name="show-password"> <label for="show-password">Show password</label>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="confirm-password-label" class="form-label" for="confirm-password">Confirm password</label> <br>
+            <input class="confirm-password" type="password" name="confirm-password"> <br>
+          </div>
+
+          <div class="form-group">
+            <input class="btn btn-register form-control form-btn" type="submit" name="submit" value="Register"> <br>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div class="col-3"></div>
+  </div>
+
 
   <?php
   include '../../shared/includes/scripts.php';
   ?>
+
+  <script src="js/checkboxPassword.js"></script>
 </body>
 
 </html>
