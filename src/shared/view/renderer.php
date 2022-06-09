03c9e3deaf6aa0e4b796da721a29ec51ebcea3de
stf <sstefanofm@gmail.com>
2022-06-08 21:48:00 -0300

    Alerts inside a container.

--- src/shared/view/renderer.php
+++ src/shared/view/renderer.php
@@ -3,7 +3,9 @@ function render_alert($success, $message)
 {
   if (isset($message)) {
 ?>
-    <div class="alert alert-<?php echo $success ? "success" : "danger" ?>"><?php echo $message ?></div>
+    <div class="container">
+      <div class="alert alert-<?php echo $success ? "success" : "danger" ?>"><?php echo $message ?></div>
+    </div>
 <?php
   }
 }
