9f2c5384407c51d8b0f4e59641282f7f3a2541e3
stf <sstefanofm@gmail.com>
2022-06-16 21:06:30 -0300

    Some bootstrap CDNs for icons and stuff.

--- src/includes/head.php
+++ src/includes/head.php
@@ -4,6 +4,8 @@
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <!-- bootstrap icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
   <!-- Google fonts -->
   <!-- Metamorphous & Special Elite -->
