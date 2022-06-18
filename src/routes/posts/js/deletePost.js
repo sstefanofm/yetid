5b351b4369ed0b230c921e61b566094c7043a974
stf <sstefanofm@gmail.com>
2022-06-17 23:25:54 -0300

    User has to confirm if them wants to delete the post.

--- src/routes/posts/js/deletePost.js
+++ src/routes/posts/js/deletePost.js
@@ -1,13 +1,18 @@
 const deleteButton = document.querySelector(".btn-delete");
 
 deleteButton.addEventListener("click", () => {
-  let formData = new FormData();
-  formData.append("id", new URLSearchParams(window.location.search).get("id"));
+  if (confirm("Are you sure you want to delete the post?")) {
+    let formData = new FormData();
+    formData.append(
+      "id",
+      new URLSearchParams(window.location.search).get("id")
+    );
 
-  fetch("controller/delete_post.php", {
-    method: "POST",
-    body: formData,
-  }).then(() => {
-    window.location.href = "../../";
-  });
+    fetch("controller/delete_post.php", {
+      method: "POST",
+      body: formData,
+    }).then(() => {
+      window.location.href = "../../";
+    });
+  }
 });
