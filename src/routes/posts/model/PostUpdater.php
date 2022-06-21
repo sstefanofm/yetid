bb0bbd84a19b095e6eb3f9fdc744935fb63013ab
stf <sstefanofm@gmail.com>
2022-06-21 03:50:55 -0300

    Validate main: title, subtitle, content. (with js);

--- src/routes/posts/model/PostUpdater.php
+++ src/routes/posts/model/PostUpdater.php
@@ -4,13 +4,13 @@ include __DIR__ . '/../../../shared/model/Database.php';
 
 class PostUpdater extends Database
 {
-  private function update_query($id, $title, $content)
+  private function update_query($id, $title, $main_subtitle, $main_content, $content)
   {
-    return "UPDATE posts SET title = '$title', content = '$content' WHERE id = '$id'";
+    return "UPDATE posts SET title = '$title', main_subtitle = '$main_subtitle', main_content = '$main_content', content = '$content' WHERE id = '$id'";
   }
 
-  function run($id, $title, $content)
+  function run($id, $title, $main_subtitle, $main_content, $content)
   {
-    return $this->connection->query($this->update_query($id, $title, $content));
+    return $this->connection->query($this->update_query($id, $title, $main_subtitle, $main_content, $content));
   }
 }
