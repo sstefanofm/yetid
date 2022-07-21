36e5ea9bd82b456a8b58044255632a1763e873dc
stf <sstefanofm@gmail.com>
2022-07-21 17:49:56 -0300

    Server ready and clean to add more code;

--- src/server/users/create_user.php
+++ src/server/users/create_user.php
@@ -1,7 +1,13 @@
 <?php
 
+// receive data;
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 $confirm_password = $_POST['confirm-password'];
+
+// try to create user in db;
+
+// send json encoded response;
 
 echo json_encode($arr);
