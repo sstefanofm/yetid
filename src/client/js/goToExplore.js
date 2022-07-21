7d1e570a095581d576d7e5a6fce5f06110fe09a4
stf <sstefanofm@gmail.com>
2022-07-21 18:34:21 -0300

    Cleaning up useless/dirty files;

--- src/client/js/goToExplore.js
+++
@@ -1,6 +0,0 @@
-const exploreButton = document.querySelector(".btn-explore");
-
-exploreButton.addEventListener("click", () => {
-  window.location.href =
-    "http://localhost/stf/yetid/src/routes/explore/explore.php";
-});
