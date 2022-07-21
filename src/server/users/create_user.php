03def9ad2cdf6469c1ce43889aead0d99e63159f
stf <sstefanofm@gmail.com>
2022-07-21 17:40:04 -0300

    Server on its own folder;

---
+++ src/server/users/create_user.php
@@ -0,0 +1,7 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$confirm_password = $_POST['confirm-password'];
+
+echo json_encode($arr);
---
+++ src/server/users/domain/Username.php
@@ -0,0 +1,10 @@
+<?php
+
+include __DIR__ . '/../../shared/domain/StringValueObject.php';
+
+class Username extends StringValueObject
+{
+  function __construct($value)
+  {
+  }
+}
