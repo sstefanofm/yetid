ee7595e6675b243ecfa24c42b6084dccbf91cd07
stf <sstefanofm@gmail.com>
2022-07-27 17:48:21 -0300

    Render home and explore buttons with function;

---
+++ src/client/shared/renderers/home_explore_buttons.php
@@ -0,0 +1,10 @@
+<?php
+
+function render_home_explore_buttons()
+{
+?>
+  <a href="http://localhost/stf/yetid/src/client/" class="btn btn-colors btn-home">Home</a>
+  <a href="http://localhost/stf/yetid/src/client/routes/explore/explore.php" class="btn btn-colors btn-explore">Explore</a>
+<?php
+}
+
