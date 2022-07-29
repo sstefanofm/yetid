ab7489837d574fda65e01071a364ff5aeb68e3e8
stf <sstefanofm@gmail.com>
2022-07-29 05:35:49 -0300

    Fixed a variable passed to the upload function;

--- src/server/posts/upload/upload_post.php
+++ src/server/posts/upload/upload_post.php
@@ -12,6 +12,6 @@ $title = $_POST['title'];
 $main_subtitle = $_POST['main_subtitle'];
 $main_content = $_POST['main_content'];
 
-$success = $uploader->run($user_id, $main_content, $title, $main_content, $main_content);
+$success = $uploader->run($user_id, $content, $title, $main_content, $main_content);
 
 echo json_encode(array("success" => $success));
